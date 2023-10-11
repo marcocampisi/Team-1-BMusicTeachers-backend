4e162445f93c250d7aa2ea500569cc865c72d2a1
marcocampisi <marco.campisi@outlook.it>
2023-10-11 12:14:50 +0200

    Add FK to Teachers

--- database/migrations/2023_10_11_095948_create_teachers_table.php
+++ database/migrations/2023_10_11_095948_create_teachers_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
             $table->text('bio');
             $table->string('cv')->nullable();
             $table->string('photo')->nullable();
