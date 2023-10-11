43b5da17c952363e2c6357126c82800267eee198
marcocampisi <marco.campisi@outlook.it>
2023-10-11 12:07:02 +0200

    Add Teachers migration

---
+++ database/migrations/2023_10_11_095948_create_teachers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->text('bio');
+            $table->string('cv')->nullable();
+            $table->string('photo')->nullable();
+            $table->unsignedInteger('phone');
+            $table->string('service');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
