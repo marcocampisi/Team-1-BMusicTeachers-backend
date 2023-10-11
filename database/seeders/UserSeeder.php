55dd3529e6b6b2792fdb4205fd71cd3c07f5afed
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-10-11 16:45:07 +0200

    add call in the Database Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,14 +5,13 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class SponsorizationSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
         
     }
 }
