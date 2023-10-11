4b0d20871d19db5131c9333a65d037152d588737
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-10-11 17:01:32 +0200

    Edit SponsorizationSeeder

--- database/seeders/SponsorizationSeeder.php
+++ database/seeders/SponsorizationSeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+//helpers
+use Illuminate\Support\Facades\Schema;
+
+//model
+use App\Models\Sponsorization;
+
 class SponsorizationSeeder extends Seeder
 {
     /**
@@ -12,7 +18,36 @@ class SponsorizationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        
+        Schema::withoutForeignKeyConstraints(function(){
+            Sponsorization::truncate();
+        });
+
+        $sponsorizations=[
+            [   
+            'name'=>'24_hours',
+            'price'=>'2.99',
+            'duration'=>24
+            ],
+            [
+            'name'=>'72_hours',
+            'price'=>'5.99',
+            'duration'=>72,
+            ],
+            [
+            'name'=>'144_hours',
+            'price'=>'9.99',
+            'duration'=>144,
+            ]];
+
+        foreach($sponsorizations as $sponsorization)
+        {
+            Sponsorization::create([
+                'name'=> $sponsorization['name'],
+                'price'=> $sponsorization['price'],
+                'duration'=> $sponsorization['duration']
+                
+            ]);
+        }
+
     }
 }
