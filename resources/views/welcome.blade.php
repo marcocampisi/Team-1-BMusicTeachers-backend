2dfd75d2a3b3027ff2e551d18628eeec5641a27d
marcocampisi <marco.campisi@outlook.it>
2023-10-12 15:58:44 +0200

    BOOTSTRAP FUNZIONA

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('main-content')
+<div class="container text-center">
+    <div class="row align-items-start">
+      <div class="col">
+        One of three columns
+      </div>
+      <div class="col">
+        One of three columns
+      </div>
+      <div class="col">
+        One of three columns
+      </div>
+    </div>
+  </div>
+
+@endsection
