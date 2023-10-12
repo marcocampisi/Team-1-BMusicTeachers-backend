69d99644cb33c817bb5dec7110d6b1e3426d8939
Andreapiani95 <andreapiani1995@gmail.com>
2023-10-12 17:46:15 +0200

    Habemus logo

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,7 @@ import './bootstrap';
 import '~resources/scss/app.scss';
 
 import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+    '../img/**'
+]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container">
                     <a class="navbar-brand" href="/">
-                        <img class="logo-bellissimo" src="/file-img-pdf/logo/logo.png" alt="">
+                        <img class="logo-bellissimo" src="{{ Vite::asset('file-img-pdf/logo/logo.png') }}" alt="">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
