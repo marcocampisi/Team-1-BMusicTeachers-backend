d1c909d968b74b54e67c7f3c60afdaf018fc6503
marcocampisi <marco.campisi@outlook.it>
2023-10-12 11:04:17 +0200

    Complete Teachers seeder

--- database/seeders/TeacherSeeder.php
+++ database/seeders/TeacherSeeder.php
@@ -2,6 +2,8 @@
 //Cos'è lo slug?
 namespace Database\Seeders;
 
+use App\Models\Teacher;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,21 @@ class TeacherSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $teachers = config('teachers');
+
+        foreach($teachers as $teacher) {
+            
+            $random_user = User::inRandomOrder()->first()->id;
+
+            Teacher::create([
+                'user_id' => $random_user,
+                'bio' => $teacher['bio'],
+                'cv' => 'uploads/documents/'.$teacher['cv'],
+                'photo' => 'uploads/images/'.$teacher['photo'],
+                'phone' => $teacher['phone'],
+                'service' => $teacher['service']
+            ]);
+
+        }
     }
 }
