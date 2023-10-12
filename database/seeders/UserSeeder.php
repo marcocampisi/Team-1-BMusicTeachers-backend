8cc90497aac0a6ef8eb90d55a7e42a52df1eb07b
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-12 10:01:23 +0200

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        $users = config('users');
+
+        foreach ($users as $user) {
+        
+            User::create([
+                'last_name'=> $user['last_name'],
+                'first_name'=> $user['first_name'],
+                'email'=> $user['email'],
+                'email_verified_at'=> null,
+                'password'=> $user['password'],
+            ]);
+         
+        }
         
     }
 }
