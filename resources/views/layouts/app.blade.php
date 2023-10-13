16abbea7e2326639310f75d7381d8361ed03d494
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-13 18:07:13 +0200

    add login form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,27 +19,27 @@
     <body>
         <div class="bg-mt">
             <header>
-                <nav class="navbar navbar-expand-lg">
+                <nav class="navbar navbar-dark navbar-expand-lg">
                     <div class="container">
                         <a class="navbar-brand" href="/">
                             <img class="logo-mt" src="{{ Vite::asset('file-img-pdf/logo/logo.png') }}" alt="">
                         </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler btn-wight" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarText">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('dashboard') }}">Home</a>
+                                <li class="nav-item ">
+                                    <a class="nav-link fs-5 text-light" href="{{ route('admin.dashboard') }}">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">I nostri teacher</a>
+                                <li class="nav-item ">
+                                    <a class="nav-link fs-5 text-light" href="{{ route('admin.teacher.index') }}">I nostri teacher</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Chi siamo</a>
+                                <li class="nav-item ">
+                                    <a class="nav-link fs-5 text-light" href="">Messaggi</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Assistenza</a>
+                                <li class="nav-item ">
+                                    <a class="nav-link fs-5 text-light" href="">Assistenza</a>
                                 </li>
                             </ul>
                             <form method="POST" action="{{ route('logout') }}">
