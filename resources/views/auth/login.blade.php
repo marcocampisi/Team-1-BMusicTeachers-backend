3ccd83c7503a7b47cb9b53a28a7e835543ff5376
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-10-14 22:51:13 +0200

    Edit Route Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,60 +3,48 @@
 @section('page-title', 'Dashboard')
 
 @section('main-content')
-<div class="container">
-
-    <div class="row justify-content-center align-items-center ms-login-card">
-        <div class="col-md-8">
-
-            <div class="card">
-                <div class="text-light fs-1 text-center">Login</div>
-
-                <div class="card-body">
-
-                    <form action="{{ route('login') }}" method="post">
-                        @csrf
-
-                        <div class="mb-2 text-light">
-                            <label for="email" class="form-label fs-4">
-                                E-mail <span class="text-danger">*</span>
-                            </label>
-
-                            <input type="email" id="email" class="ms-form-control fs-5 text-light @error('email') is-invalid @enderror" name="email"  required>
+<section>
+    <div class="form-box">
+        <div class="form-value">
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <h2>Login</h2>
+                <div class="inputbox">
+                    <ion-icon name="mail-outline"></ion-icon>
+                    <label for="email">Email<span class="text-danger">*</span></label>
+                    <input type="email" id="email" class="ms-form-control fs-5 text-light @error('email') is-invalid @enderror" name="email"  required>
                             @error('email')
                                 <span class="invalid-feedback">{{ $message }}</span>
                             @enderror
-                        </div>
-
-                        <div class="mb-2 text-light">
-                            <label for="password" class="form-label fs-4">Password <span class="text-danger">*</span></label>
-                            <input type="password" id="password" class="ms-form-control fs-5 text-light ms-input @error('password') is-invalid @enderror" name="password" required>
+                </div>
+                <div class="inputbox">
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                    <label for="password">Password<span class="text-danger">*</span></label>
+                    <input type="password" id="password" class="ms-form-control fs-5 text-light ms-input @error('password') is-invalid @enderror" name="password" required>
                             @error('password')
                                 <span class="invalid-feedback">{{ $message }}</span>
-                            @enderror
-                        </div>
+                            @enderror                  
+                </div>
+                <div class="mb-1 text-light">
+                    <p class="fs-1 fw-light fs-3">I campi contrassegnati <span class="text-danger">*</span> sono obbligatori.</p>
+                </div>
 
-                        <div class="mb-3 text-light">
-                            <p class="fs-6 fw-light fs-5">I campi contrassegnati <span class="text-danger">*</span> sono obbligatori.</p>
-                        </div>
-
-                        <div class="mb-3 text-light">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="remember" name="remember">
-                                <label class="form-check-label" for="remember">Ricordami</label>
-                                
-                            </div>
-                        </div>
-
-
-                        <button type="submit" class="btn btn-outline-primary">Login</button>
-
-                    </form>
+                <div class="forget">
+                    <label for="remember">
+                        <input type="checkbox" class="form-check-input" id="remember" name="remember"> Ricordami
+                    </label>
+                    <label>
+                        <a href="#">Forgot Password?</a>
+                    </label>
+                </div>    
+                <button type="submit">Login</button>
+                <div class="register">
+                    <p>Don't have a account ? <a href="#">Register</a></p>
+                </div>
 
                 </div>
-            </div>
-
+            </form>
         </div>
     </div>
-
-</div>
+</section>
 @endsection
