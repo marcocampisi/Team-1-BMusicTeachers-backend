51a7212e1ba73e2a3a2d4a2abe984bc02b213883
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-15 10:11:16 +0200

    add teachers img and initial responsive

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -4,19 +4,21 @@
 
 @section('main-content')
     <h1 class="text-center text-warning my-4">Index di Teacher</h1>
-    <div class="row">
-        @foreach ($teachers->all() as $teacher)
-            <div class="col-12 col-sm-3">
-                <div class="card mb-4">
-                    <img src="https://picsum.photos/1000" class="card-img-top" alt="...">
-                    <div class="card-body my-card-slug">
-                        <h5 class="card-title">{{ $teacher->user->first_name }}</h5>
-                        <p class="card-text">{{ $teacher->bio }}</p>
-                        <a href="{{ route('admin.teachers.show', ['teacher' => $teacher]) }}" class="btn btn-primary">Show</a>
-                        <a href="{{ route('admin.teachers.edit', ['teacher' => $teacher]) }}" class="btn btn-warning">Edit</a>
+    <div class="container">
+        <div class="row row-gap-3">
+            @foreach ($teachers as $teacher)
+                <div class="col-12 col-md-3 col-sm-4">
+                    <div class="card mb-4 h-100">
+                        <img src="/storage/{{$teacher->photo  }}" class="card-img-top" alt="...">
+                        <div class="card-body my-card-slug bg-light">
+                            <h5 class="card-title">{{ $teacher->user->first_name }} {{ $teacher->user->last_name }}</h5>
+                            <p class="card-text">{{ $teacher->bio }}</p>
+                            <a href="{{ route('admin.teachers.show', ['teacher' => $teacher]) }}" class="btn btn-primary">Show</a>
+                            <a href="{{ route('admin.teachers.edit', ['teacher' => $teacher]) }}" class="btn btn-warning">Edit</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 @endsection
