acb5f02f0785927e5ca2eba6d92abfe124f8e8eb
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-16 12:51:27 +0200

    index api

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 
 //Models
-use App\Models\Teacher;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 class TeacherController extends Controller
 {
@@ -14,14 +15,28 @@ class TeacherController extends Controller
     public function index()
     {
         //
-        $teachers=Teacher::join('sponsorization_teacher', 'teacher_id', '=', 'sponsorization_teacher.teacher_id')
-            ->with('subjects', 'ratings', 'reviews', 'sponsorization')
-            ->orderByRaw('sponsored_until >= NOW() DESC, sponsored_until')->paginate(10);
+        // $teachers=Teacher::join('sponsorization_teacher', 'teacher_id', '=', 'sponsorization_teacher.teacher_id')
+        //     ->with('subjects', 'ratings', 'reviews', 'sponsorization')
+        //     ->orderByRaw('sponsored_until >= NOW() DESC, sponsored_until')->paginate(10);
+
+        $sponsoredTeachers=Teacher::with('subjects', 'ratings', 'reviews', 'sponsorization')
+            ->join('sponsorization_teacher', 'teachers.id', '=', 'sponsorization_teacher.teacher_id')
+            ->orderByRaw('sponsorization_teacher.sponsored_until >= NOW() DESC, sponsorization_teacher.sponsored_until');
+
+        $nonSponsoredTeachers = Teacher::with('subjects', 'ratings', 'reviews', 'sponsorization')
+        ->whereNotExists(function ($query) {
+            $query->select(DB::raw(1))
+                ->from('sponsorization_teacher')
+                ->whereColumn('teachers.id', 'sponsorization_teacher.teacher_id');
+        });
+        
+        $teachers = $sponsoredTeachers->union($nonSponsoredTeachers);
 
         dd($teachers);
+
         return response()->json([
             'success' => true,
-            'results' => $teachers
+            // 'results' => $teachers
         ]);
     }
 
