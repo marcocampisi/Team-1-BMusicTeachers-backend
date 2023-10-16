7c661635c7b1fe742b95998b25fbe99d83270eb7
marcocampisi <marco.campisi@outlook.it>
2023-10-16 11:24:10 +0200

    Remove comments from Api/TeacherController

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -32,36 +32,4 @@ class TeacherController extends Controller
 
         ]);
     }
-
-
-    /*
-    public function index()
-    {
-        
-
-        return response()->json([
-            'success' => true,
-            'results' => $posts
-        ]);
-    }
-
-    public function show(string $slug)
-    {
-        $post = Post::where('slug', $slug)->first();
-
-        if ($post) {
-            return response()->json([
-                'success' => true,
-                'results' => $post
-            ], 200);
-        }
-        else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Not found'
-            ], 404);
-        }
-    }
-
-    */
 }
