8f88718d6b032187d1834b09963e32747f9393c5
marcocampisi <marco.campisi@outlook.it>
2023-10-16 17:50:50 +0200

    Changes to show

--- resources/views/admin/teachers/show.blade.php
+++ resources/views/admin/teachers/show.blade.php
@@ -3,8 +3,15 @@
 @section('page-title', 'Show di Teacher')
 
 @section('main-content')
-    <h1 class="text-white">{{ $teacher->user->first_name }}</h1>
-    <p class="text-white">{{ $teacher->bio }}</p>
-    <p class="text-white">+39 {{ $teacher->phone }}</p>
-    <button class="btn btn-danger">SLUG</button>
+    <div class="container">
+        <h1 class="text-white fw-bold">{{ $teacher->user->first_name }} <span class="text-secondary fw-medium ">{{ $teacher->user->last_name }}</span></h1>
+        <p class="text-white">{{ $teacher->bio }}</p>
+        <p class="text-white">+39 {{ $teacher->phone }}</p>
+        <button class="btn btn-warning col-1 me-2">
+            <a class="text-white" href="{{ route('admin.teachers.edit', ['teacher' => $teacher->id]) }}">Modifica</a>
+        </button>
+        <button class="btn btn-danger col-1">
+            <a class="text-white" href="{{ route('admin.teachers.destroy', ['teacher' => $teacher->id]) }}">Elimina</a>
+        </button>
+    </div>
 @endsection
