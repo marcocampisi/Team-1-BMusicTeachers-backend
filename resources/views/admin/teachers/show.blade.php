b6dd5efb7155ad3d9fc06fb51708fdf2e9d9cdd0
marcocampisi <marco.campisi@outlook.it>
2023-10-16 18:10:13 +0200

    Fix buttons

--- resources/views/admin/teachers/show.blade.php
+++ resources/views/admin/teachers/show.blade.php
@@ -7,11 +7,14 @@
         <h1 class="text-white fw-bold">{{ $teacher->user->first_name }} <span class="opacity-50 fw-medium ">{{ $teacher->user->last_name }}</span></h1>
         <p class="text-white">{{ $teacher->bio }}</p>
         <p class="text-white">+39 {{ $teacher->phone }}</p>
-        <button class="btn btn-warning col-1 me-2">
+        <button class="btn btn-warning col-2 mb-3">
             <a class="text-white" href="{{ route('admin.teachers.edit', ['teacher' => $teacher->id]) }}">Modifica</a>
         </button>
-        <button class="btn btn-danger col-1">
-            <a class="text-white" href="{{ route('admin.teachers.destroy', ['teacher' => $teacher->id]) }}">Elimina</a>
-        </button>
+        <form action="{{ route('admin.teachers.destroy', ['teacher' => $teacher->id]) }}" method="post">
+            @csrf
+            @method('DELETE')
+
+            <button class="btn btn-danger col-2">Elimina</button>
+        </form>
     </div>
 @endsection
