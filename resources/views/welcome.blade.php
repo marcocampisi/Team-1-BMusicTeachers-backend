d94ad7a36f6250fbe1a4eb902d7f242bfc95e59a
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-16 23:48:34 +0200

    add welcame app descrirtion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.guest')
 
 @section('main-content')
-<h1 class="text-white text-center fs-1">Welcome</h1>
+
+
+
+<div class="row">
+  <div class="col-md-6 col-12 welcome-blur">
+      <h1 class="text-white fs-1">Benvenuto!</h1>
+      <h2 class=" text-start">
+        <strong class="text-success">musicisti in rete</strong> è una piattaforma musicale unica che permette agli artisti indipendenti di condividere la propria musica con il mondo.
+      </h2>
+      <p class="  fs-4 text-light">
+        "La nostra app è la chiave per connettere appassionati di musica con insegnanti esperti, offrendo lezioni straordinarie per imparare a suonare gli strumenti che ami. Scopri il mondo della musica con i migliori insegnanti al tuo fianco. Inizia a suonare oggi stesso!"
+      </p>
+      <p class="text-warning fs-4">
+        ricordati di registrarti per accedere a tutte le funzionalità riservate ai membri
+      </p>
+      
+    </div>
+
+
+</div>
+
+
 
 @endsection
