569920eab4700c8ed657e589f91a5cf3478c5e14
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-10-16 22:35:34 +0200

    Change unicode star

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -10,7 +10,12 @@
                 <div class="col-12 col-md-4 col-lg-3 ">
                     <div class="card mb-4 h-100 ms-bg-index-card border text-light position-relative text-center">
                         <img src="/storage/{{ $teacher->photo }}" class="card-img-top" alt="{{ $teacher->user->first_name }}">
-                        <div class="wishlist bg-success">&hearts;</div>
+                        {{-- <div class="wishlist">&#9733;</div> --}}
+                        <div class="wishlist">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                <path fill="black" d="M12 2.3l2.7 7.7h7.6l-6 4.6 2.7 7.7-7-5.3-7 5.3 2.7-7.7-6-4.6h7.6z"/>
+                            </svg>
+                        </div>                        
                         <div class="card-body d-flex flex-column justify-content-between my-card-slug">
                             <h5 class="card-title">{{ $teacher->user->first_name }} {{ $teacher->user->last_name }}</h5>
                             <p class="card-text">{{ $teacher->bio }}</p>
