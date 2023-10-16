9da33f152cd356e67cff447f9386d7f30e876849
marcocampisi <marco.campisi@outlook.it>
2023-10-16 17:52:18 +0200

    Fix text color

--- resources/views/admin/teachers/show.blade.php
+++ resources/views/admin/teachers/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('main-content')
     <div class="container">
-        <h1 class="text-white fw-bold">{{ $teacher->user->first_name }} <span class="text-secondary fw-medium ">{{ $teacher->user->last_name }}</span></h1>
+        <h1 class="text-white fw-bold">{{ $teacher->user->first_name }} <span class="opacity-50 fw-medium ">{{ $teacher->user->last_name }}</span></h1>
         <p class="text-white">{{ $teacher->bio }}</p>
         <p class="text-white">+39 {{ $teacher->phone }}</p>
         <button class="btn btn-warning col-1 me-2">
