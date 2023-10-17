a8db1c4b4223a6aae253dc673fbbe6673ef0a168
Andreapiani95 <andreapiani1995@gmail.com>
2023-10-17 10:46:50 +0200

    Database seeder fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
             SubjectSeeder::class,
             ReviewSeeder::class,
             SubjectTeacherSeeder::class,
+            SponsorizationTeacherSeeder::class,
+            RatingTeacherSeeder::class,
           ]);
     }
 }
