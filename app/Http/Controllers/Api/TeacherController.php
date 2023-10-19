93ea6df0bef93bfdd1acf5e79eb078f307e2310a
Andreapiani95 <andreapiani1995@gmail.com>
2023-10-19 12:09:05 +0200

    api for filtered teachers

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -28,6 +28,19 @@ class TeacherController extends Controller
         ]);
     }
 
+    public function search(Request $request)
+    {
+    $searchQuery = $request->get('q');
+
+    $teachers = Teacher::where('first_name', 'like', "%{$searchQuery}%")
+        ->orWhere('last_name', 'like', "%{$searchQuery}%")
+        ->get();
+
+    return response()->json([
+        'results' => $teachers
+    ]);
+    }
+
     public function show(Teacher $teacher)
     {
 
