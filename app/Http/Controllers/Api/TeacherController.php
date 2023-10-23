c8fbc82a442f740c9326faf276878a247565a670
Andreapiani95 <andreapiani1995@gmail.com>
2023-10-23 11:37:34 +0200

    commit

--- app/Http/Controllers/Api/TeacherController.php
+++ app/Http/Controllers/Api/TeacherController.php
@@ -58,7 +58,7 @@ class TeacherController extends Controller
             'results' => $teachers
         ]);
     }
-/*
+
     public function chart(string $id)
     {
         $teacherDataset = Teacher::with('messages', 'ratings', 'reviews')
@@ -73,18 +73,14 @@ class TeacherController extends Controller
             'results' => $teacherDataset
         ]);
     }
-*/
+
     public function show(string $id)
     {
-        $teacher = Teacher::with('subjects', 'ratings', 'reviews', 'sponsorization', 'user', 'messages')
+        $teacher = Teacher::with('subjects', 'ratings', 'reviews', 'sponsorization', 'user')
         ->leftJoin('sponsorization_teacher', 'teachers.id', '=', 'sponsorization_teacher.teacher_id')
         ->leftJoin('users', 'teachers.user_id', '=', 'users.id')
-        ->leftJoin('rating_teacher', 'teachers.id', '=', 'rating_teacher.teacher_id')
-        ->leftJoin('ratings', 'rating_teacher.rating_id', '=', 'ratings.id')
-        ->leftJoin('reviews', 'teachers.id', '=', 'reviews.teacher_id')
-        ->leftJoin('messages', 'teachers.id', '=', 'messages.teacher_id')
         ->where('teachers.id', $id)
-        ->select('teachers.*', 'users.first_name', 'users.last_name', 'sponsorization_teacher.sponsored_until', 'ratings.*', 'reviews.*', 'messages.*')
+        ->select('teachers.*', 'users.first_name', 'users.last_name', 'sponsorization_teacher.sponsored_until')
         ->first();
     
         if ($teacher) {
