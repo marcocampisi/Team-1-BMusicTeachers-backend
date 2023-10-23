ffce9f5e50301f871aa02e15c03f1755619dd5a2
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-10-23 16:28:40 +0200

    change chart controller

--- app/Http/Controllers/Api/ChartController.php
+++ app/Http/Controllers/Api/ChartController.php
@@ -11,13 +11,7 @@ class ChartController extends Controller
     public function show(string $id)
     {
         $teacherDataset = Teacher::with('messages', 'ratings', 'reviews', 'user')
-            ->leftJoin('users', 'users.id', '=', 'teachers.user_id')
-            ->leftJoin('rating_teacher', 'teachers.id', '=', 'rating_teacher.teacher_id')
-            ->leftJoin('ratings', 'rating_teacher.rating_id', '=', 'ratings.id')
-            ->leftJoin('messages', 'messages.teacher_id', '=', 'teachers.id')
-            ->leftJoin('reviews', 'reviews.teacher_id', '=', 'teachers.id')
             ->where('teachers.id', $id)
-            ->select('teachers.*', 'messages.*', 'ratings.*', 'rating_teacher.*', 'reviews.*')
             ->get();
 
         return response()->json([
