a3c88a9daa0e6b6168e81e97edc5e6f87d7ff922
marcocampisi <marco.campisi@outlook.it>
2023-10-27 11:53:50 +0200

    Fix login title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
     <div class="form-box pt-4 pb-3">
         <form action="{{ route('login') }}" method="post">
             @csrf
-            
-            <h2>Login</h2>
+
+            <h2 class="text-center text-white">Login</h2>
             <div class="inputbox">
                 <ion-icon name="mail-outline"></ion-icon>
 
                 <input type="email" id="email" class="text-light @error('email') is-invalid @enderror" name="email"  required required autocomplete="off">
                 <label for="email">
-                   
+
                     Email
                     <span class="text-danger">*</span>
                 </label>
@@ -24,13 +24,13 @@
             </div>
             <div class="inputbox">
                 <ion-icon name="lock-closed-outline"></ion-icon>
-                
+
                 <input type="password" id="password" class="ms-form-control fs-5 text-light ms-input @error('password') is-invalid @enderror" name="password" required required autocomplete="off">
                 <label for="password">Password<span class="text-danger">*</span></label>
                 @error('password')
                     <span class="invalid-feedback">{{ $message }}</span>
-                @enderror   
-                               
+                @enderror
+
             </div>
             <div class="mb-1 text-light">
                 <p class="fw-light ms-small-text">I campi contrassegnati <span class="text-danger">*</span> sono obbligatori.</p>
@@ -41,16 +41,14 @@
                     <input type="checkbox" class="form-check-input" id="remember" name="remember"> Ricordami
                 </label>
                 <label>
-                    <a href="#">Forgot Password?</a>
+                    <a href="#">Password Dimenticata?</a>
                 </label>
-            </div>    
+            </div>
             <button class="btn-log" type="submit">Login</button>
             <div class="register">
-                <p>Don't have a account ?  <a class="mt-2" href="{{ route('register') }}">Register</a></p>
+                <p>Non hai un aaccount?  <a class="mt-2" href="{{ route('register') }}">Registrati</a></p>
             </div>
-
-            </div>
-        </form> 
+        </form>
     </div>
 </section>
 @endsection
