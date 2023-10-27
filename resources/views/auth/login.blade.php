9e0e2cb1bc407164bb6ddbc94af655d4fb024fc9
Marco Campisi <marco.campisi@outlook.it>
2023-10-27 16:34:58 +0200

    Fix navbar register and login 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.guest')
+@extends('layouts.app')
 
 @section('page-title', 'Dashboard')
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.guest')
+@extends('layouts.app')
 
 @section('main-content')
     <div class="form-section">
