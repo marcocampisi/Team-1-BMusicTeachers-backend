484e5b0d42f35e1710ea95061fff152d340817df
SimonaCaruso87 <caruso.simona87@gmail.com>
2023-10-27 09:46:19 +0200

    Add card responsive

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -4,10 +4,11 @@
 
 @section('main-content')
 
+{{-- INIZIO CSS CARD SPONSORIZZAZIONI --}}
 {{-- <div>
     <img class="back-dash" src="{{ Vite::asset('/resources/img/file-img-pdf/png/note-2.png') }}" alt="note">
 </div> --}}
-<div class="container-card">
+{{-- <div class="container-card">
     <h5 class="text-center">Scegli il piano che fa per te</h5>  
 </div>
   <div class="container-card">
@@ -21,15 +22,6 @@
         <a href="{{ route('user.checkout', ['sponsorization' => 1]) }}" class="button-card">Compra Adesso</a>
       </li>    
     </ul>
-      {{-- <ul class="col-price stand-out">
-      <li class="header">BASIC</li>
-        <li class="price"><span>$20</span><i>per month</i></li>
-      <li>5,000 MB</li>
-      <li>15 Email</li>
-      <li>10 Databases</li>
-      <li>Support</li>
-      <li class="get-it"><button type="button">Get it now</button></li>
-      </ul> --}}
     <ul class="col-price">
       <li class="header">MEDIO</li>
         <li class="price"><span>€5.99</span><i>per 3 giorni</i></li>
@@ -49,8 +41,68 @@
       </li>       
     </ul>
     </div>
+  </div> --}}
+  <div class="container-card">
+    <h5 class="text-center">Scegli il piano che fa per te</h5>
   </div>
+  <div class="pricing-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-6">
+                <div class="single-price">
+                    <div class="price-header">
+                        <h3 class="title">Base</h3>
+                    </div>
+                    <div class="price-value">
+                        <div class="value">
+                            <span class="currency">$</span> <span class="amount">2.<span>99</span></span> <span class="month">/month</span>
+                        </div>
+                    </div>
+                    <ul class="deals">
+                        <li>Giornaliero.</li>
+                        <li>Nessun Supporto.</li>
+                    </ul><a href="{{ route('user.checkout', ['sponsorization' => 1]) }}" class="button-card">Compra Adesso</a>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="single-price">
+                    <div class="price-header">
+                        <h3 class="title">Medio</h3>
+                    </div>
+                    <div class="price-value">
+                        <div class="value">
+                            <span class="currency">$</span> <span class="amount">5.<span>99</span></span> <span class="month">/month</span>
+                        </div>
+                    </div>
+                    <ul class="deals">
+                        <li>Tris Gionaliero.</li>
+                        <li>Nessun Supporto.</li>
+                    </ul><a href="{{ route('user.checkout', ['sponsorization' => 2]) }}" class="button-card">Compra Adesso</a>
 
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <div class="single-price">
+                    <div class="price-header">
+                        <h3 class="title">Professionale</h3>
+                    </div>
+                    <div class="price-value">
+                        <div class="value">
+                            <span class="currency">$</span> <span class="amount">9.<span>99</span></span> <span class="month">/month</span>
+                        </div>
+                    </div>
+                    <ul class="deals">
+                        <li>Settimanale.</li>
+                        <li>Supporto Incluso.</li>
+                    </ul><a href="{{ route('user.checkout', ['sponsorization' => 3]) }}" class="button-card">Compra Adesso</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>  
+{{-- FINE CSS CARD SPONSORIZZAZIONI --}}
+
+{{-- INIZIO CSS GRAFICI --}}
 <h3 class="mt-3 text-light">Grafici</h3>
 <label for="timeFrameSelect" class="text-light">Seleziona il periodo:</label>
 <select id="timeFrameSelect">
@@ -61,9 +113,13 @@
 <div class="w-50">
   <canvas id="myChart" width="400" height="200"></canvas>
 </div>
+{{-- FINE CSS GRAFICI --}}
+
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
 <script>
+
+// INIZIO CODICE JAVASCRIPT PER GESTIONE GRAFICI
    let data = @json($monthlyAverages);
     let labels = data.map(function(item) {
         return item.month;
@@ -136,5 +192,7 @@
     });
 
     updateChart('month');
+// FINE CODICE JAVASCRIPT PER GESTIONE GRAFICI
+
 </script>
 @endsection
